ad58e6024fcc3a3e35085d16a1873a497ba6f754
Andrés Pujol <andrespujol@gmail.com>
2021-05-25 23:24:33 -0300

    se arregló la subida de imagenes en crear productos

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -4,9 +4,8 @@ const multer = require('multer');
 
 
 const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, '../public/images'); //Acá había solo un punto, pero en router teníamos dos. Así que lo puse igual.
-	},
+	destination: path.resolve(__dirname, '../public/images'), //Acá había solo un punto, pero en router teníamos dos. Así que lo puse igual.
+	
 	filename: (req, file, cb) => {
 		let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
 		cb(null, fileName);
