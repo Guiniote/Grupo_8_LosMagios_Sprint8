9bc77f72f1565790092f2e5d8e53c53611f56cba
gabodmc <gabrielmc@live.com.ar>
2021-05-27 17:59:22 -0300

    Volvi a sacarle el callback al multer pq no estaba funcionando como queria. Le meti un template al profile para mostrarlo mas lindo - GMC

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -15,9 +15,8 @@ const storageProduct = multer.diskStorage({
 const uploadProduct = multer({ storage: storageProduct });
 
 const storageAvatar = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, './public/images/avatars');
-	},
+	destination: path.resolve(__dirname, '../public/images/avatars'),
+
 	filename: (req, file, cb) => {
 		let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
 		cb(null, fileName);
