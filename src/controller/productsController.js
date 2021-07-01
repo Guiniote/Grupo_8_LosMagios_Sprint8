083e6b0aaf3802cd9d87e6e5eff74724753d65bb
Guiniote <guiniote@gmail.com>
2021-07-01 01:52:39 -0300

    Subo los pocos avances en product -GG

--- src/controller/productsController.js
+++ src/controller/productsController.js
@@ -1,73 +1,109 @@
 //const jsonDB = require('../model/productsModel');
 //const productsModel = jsonDB('products');
-const db = require('../database/models');
+//const db = require('../database/models');
+
+const { Product, Brand, Category, Image  } = require('../database/models');
+const { Op } = require("sequelize");
 
 
 
 const productsController = {
 
 // Función que muestra el formulario de crear Productos
-    create: (req, res) => {
-        res.render('products/createProducts');
+    create: async (req, res) => {
+        let brands = await Brand.findAll();
+        let categories = await Category.findAll();
+        res.render('products/createProducts', { brands, categories });
     },
-
-// Función que simula el almacenamiento, en este caso en array
+    
+// // Función que simula el almacenamiento, en este caso en array
+//     store: (req, res) => {
+//         const product = req.body;
+//         product.image = req.file ? req.file.filename : '';
+//         productsModel.create(product);
+//         res.redirect('/products/productList');
+//     },
     store: (req, res) => {
         const product = req.body;
-        product.image = req.file ? req.file.filename : '';
-        productsModel.create(product);
-        res.redirect('/products/productList');
-    },
+        
+        //product.image = req.file ? req.file.filename : '';
+        Product.create(req.body)
+            .then(productStored => {                
+                productStored.addCategories(req.body.category); 
+                productStored.addBrands(req.body.brand); 
+                res.redirect('/products/productList');
+            })
+        .catch(error => res.send(error));
+    },  
+    
+    // const product = await Product.create(req.body)
+    //         //.then(productStored => {                
+    //             await product.addCategories(req.body.category); 
+    //             await product.addBrands(req.body.brand); 
+    //             res.redirect('/products/productList');
 
-// Función que muestra la información almacenada
-    show: (req, res) => {
-        const product = productsModel.find(req.params.id);
-        if (product) {
-            res.render('products/productDetail', { product });
-        } else {
-            res.render('error404');
-        }
-    },
+// // Función que muestra la información almacenada
+//     show: (req, res) => {
+//         const product = productsModel.find(req.params.id);
+//         if (product) {
+//             res.render('products/productDetail', { product });
+//         } else {
+//             res.render('error404');
+//         }
+//     },
 
-// Función que borra información almacenada
-    destroy: (req, res) => {
-        productsModel.delete(req.params.id);
-        res.redirect('/products/productList');
-    },
+// // Función que borra información almacenada
+//     destroy: (req, res) => {
+//         productsModel.delete(req.params.id);
+//         res.redirect('/products/productList');
+//     },
 
 //Función para listar los productos
-    list: (req, res) => {
-        const products = productsModel.all();
-        res.render('products/productList', { products });
-    },
+    // list: (req, res) => {
+    //     const products = productsModel.all();
+    //     res.render('products/productList', { products });
+    //  },
 
-// Función para traer datos los productos para editar
-    edit: (req, res) => {
-        let product = productsModel.find(req.params.id);
-        if (product) {
-            res.render('products/editProducts', { product });
-        } else {
-            res.render('error404');
-        }
-    },
+    list: async (req, res) => {        
+        if (req.query) {            
+        const query = req.query;            
+        const productNameKeyword = query.product_name ? query.product_name: '';
+        let products = await Product.findAll({
+            where: { name: { [Op.substring]: productNameKeyword }} 
+        });
+        return res.render('products/productList', { products });
+    } else {
+        let products = await Product.findAll();        
+        return res.render('products/productList', { products });
+    }},
+
+// // Función para traer datos los productos para editar
+//     edit: (req, res) => {
+//         let product = productsModel.find(req.params.id);
+//         if (product) {
+//             res.render('products/editProducts', { product });
+//         } else {
+//             res.render('error404');
+//         }
+//     },
         
-// Función para actualizar información editada de los producto
-    update: (req, res) => {
-        let  product = req.body;
-        product.id = req.params.id;
-        product.image = req.file ? req.file.filename : req.body.old_image;
-        if (req.body.image===undefined) {
-            product.image = req.body.old_image
-        }
-        delete product.oldImage;
-        productsModel.update(product);
-        res.redirect("/products/productDetail/" + req.params.id);
-    }, 
+// // Función para actualizar información editada de los producto
+//     update: (req, res) => {
+//         let  product = req.body;
+//         product.id = req.params.id;
+//         product.image = req.file ? req.file.filename : req.body.old_image;
+//         if (req.body.image===undefined) {
+//             product.image = req.body.old_image
+//         }
+//         delete product.oldImage;
+//         productsModel.update(product);
+//         res.redirect("/products/productDetail/" + req.params.id);
+//     }, 
 
-// Función para guardar y mostrar el carrito de compras
-    cart: (req, res) => {
-        res.render('products/productCart');
-    }
+// // Función para guardar y mostrar el carrito de compras
+//     cart: (req, res) => {
+//         res.render('products/productCart');
+//     }
 };
 
 
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -4,28 +4,28 @@ const productsController = require('../controller/productsController');
 
 // Middlewares
 const {uploadProduct} = require('../middlewares/multerMiddleware');
-const {validateEditProduct} = require('../middlewares/validationsMiddleware');
-const userLoggedMiddleware = require('../middlewares/userLoggedMiddleware');
+// const {validateEditProduct} = require('../middlewares/validationsMiddleware');
+// const userLoggedMiddleware = require('../middlewares/userLoggedMiddleware');
 
 // Carrito
-router.get('/productCart', productsController.cart);
-router.post('/productCart', productsController.cart);
+// router.get('/productCart', productsController.cart);
+// router.post('/productCart', productsController.cart);
 
 // Lista de productos
 router.get('/productList', productsController.list);
 
 // Detalle de productos
-router.get('/productDetail/:id', productsController.show);
+//router.get('/productDetail/:id', productsController.show);
 
 // Creación de productos
-router.get('/createProducts', userLoggedMiddleware, productsController.create);
-router.post('/store', uploadProduct.single('image'), productsController.store);
+ router.get('/createProducts', /*userLoggedMiddleware,*/ productsController.create);
+ router.post('/store', uploadProduct.single('image'), productsController.store);
 
 // Edición de productos
-router.get('/editProducts/:id', productsController.edit);
-router.post ('/editProducts/:id', uploadProduct.single('image'), validateEditProduct, productsController.update);
-router.get('/:id', productsController.show);
-router.delete('/productDetail/:id', productsController.destroy);
+// router.get('/editProducts/:id', productsController.edit);
+// router.post ('/editProducts/:id', uploadProduct.single('image'), validateEditProduct, productsController.update);
+// router.get('/:id', productsController.show);
+// router.delete('/productDetail/:id', productsController.destroy);
 
 
 
