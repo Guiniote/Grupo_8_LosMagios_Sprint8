d3fb3078e82cc9112472d5fc5bd453af819cedf9
Guiniote <guiniote@gmail.com>
2021-07-29 00:44:20 -0300

    Otro intento de que funcione el heroku -GG

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -16,5 +16,20 @@ module.exports =
 
     migrationStorage: "sequelize",
     migrationStorageTableName: "migrations"
-    }
+    },
+
+    production: {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
+        "dialect": process.env.DB_DIALECT,
+    
+        seederStorage: "sequelize",
+        seederStorageTableName: "seeds",
+    
+        migrationStorage: "sequelize",
+        migrationStorageTableName: "migrations"
+        }
 }
