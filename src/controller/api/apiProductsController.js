2fcd6dc424ef94f3acef23537f632a15c40dcce9
Guiniote <guiniote@gmail.com>
2021-08-01 13:49:31 -0300

    Subo APIs de productos terminadas -GG

--- src/controller/api/apiProductsController.js
+++ src/controller/api/apiProductsController.js
@@ -9,24 +9,82 @@ const apiProductsController = {
         let response = {
             count: 0,
             countByCategory: {},
-            products: [],
+            products: [],            
             status: 0,
         };
         
-        let products = await Product.findAll({ 
-            include: ['brand', 'category', 'images']}
-        );
+        try{
+            let products = await Product.findAndCountAll({ 
+                include: ['brand', 'category', 'images']}
+            );
 
-        let categories = await Category.findAll();
+            response.count = products.rows.length;
 
-        response.count = products.length;
-        response.products = products;
-        response.status = 200;
 
+            let categories = await Category.findAll();
+                    
+            categories.forEach( category => {
+                response.countByCategory[category.name] = 0;            
+            });
+
+            response.products = products.rows.map( row => {
+                response.countByCategory[row.category.name]++;
+
+                let product = {
+                    id: row.id,
+                    name: row.name,
+                    description: row.description,
+                    images: row.images.map(image => image.name),
+                    detail: 'http://' + req.headers.host + `/api/products/${row.id}`
+                }
+                return product;
+            })
+
+            response.status = 200;
+        }
+        
+        catch{
+            response.status = 500;
+        }
+
+        
         return res.json(response);
             
     },
-    
+
+    listById: async (req, res) => {
+
+        let response = {
+            product: {},
+            relations: [],
+            images: [],
+            status: 0,
+        };
+
+        try{
+            let product = await Product.findByPk(req.params.id, { 
+                include: ['brand', 'category', 'images']}
+            );
+            
+            response.product = product
+
+            let imagenes = [];
+            product.images.forEach(image => { imagenes.push(image.name)})
+            response.relations = [{brand: product.brand.name}, {category: product.category.name}, {images: imagenes}]
+
+            response.images = imagenes.map(image => 'http://' + req.headers.host + `/images/${image}`)
+
+            response.status = 200;
+        }
+
+        catch {
+            response.status = 500;            
+        }
+
+        
+        return res.json(response);
+
+    }
 };
 
 
