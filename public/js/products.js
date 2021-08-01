b65b5825281f66d23dca03791ba096242190c5f8
gabodmc <gabrielmc@live.com.ar>
2021-08-01 13:11:53 -0300

    Subiendo validaciones del front para createProducts - GMC

---
+++ public/js/products.js
@@ -0,0 +1,216 @@
+window.addEventListener("load", function() {
+    
+    let formulario = document.querySelector (".form");
+    let camposErrores = document.querySelectorAll("small");   
+    let errors = [];
+    const acceptedExtensions = /(jpg|jpeg|png|gif)$/;
+    const passwordChars = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/;
+    let productName = document.querySelector("#name");
+    let productModel = document.querySelector("#brand");
+    let productDescription = document.querySelector("#description");
+    let productKeywords = document.querySelector("#keywords");
+    let productPrice = document.querySelector("#price");
+    let productDiscount = document.querySelector("#discount");
+    let productStock = document.querySelector("#stock");
+    let productStockmin = document.querySelector("#stockMin");
+    let productStockmax = document.querySelector("#stockMax");
+
+
+    productName.addEventListener ("blur", function(e) {
+
+        errors = [];
+            
+        if(nombre.value == '') {                
+            errors.push({ name: 'productName', msg: 'Recordá ingresar un nombre' });
+        } else if (nombre.value.length < 2) {                
+            errors.push({ name: 'productName', msg: 'El nombre debe tener al menos 2 caracteres' });
+        } 
+        
+        if(errors.length == 0) {                
+            camposErrores[0].innerText = '';
+        } else {
+            for (let i = 0; i < errors.length; i++) {
+                let lineaError = document.querySelector("small." + errors[i].name);                
+                lineaError.innerText = errors[i].msg;            
+            }            
+        }  
+    });
+
+    productModel.addEventListener ("blur", function(e) {
+
+        errors = [];
+            
+        if(nombre.value == '') {                
+            errors.push({ name: 'productModel', msg: 'Recordá ingresar un modelo' });
+        };
+        
+        if(errors.length == 0) {                
+            camposErrores[0].innerText = '';
+        } else {
+            for (let i = 0; i < errors.length; i++) {
+                let lineaError = document.querySelector("small." + errors[i].name);                
+                lineaError.innerText = errors[i].msg;            
+            }            
+        };  
+    });
+
+    productDescription.addEventListener ("blur", function(e) {
+
+        errors = [];
+            
+        if(nombre.value == '') {                
+            errors.push({ name: 'productDescription', msg: 'Recordá ingresar una descripción' });
+        } else if (nombre.value.length < 20) {                
+            errors.push({ name: 'productDescription', msg: 'La descripción debe contar con al menos 20 caracteres'});
+        } 
+        
+        if(errors.length == 0) {                
+            camposErrores[0].innerText = '';
+        } else {
+            for (let i = 0; i < errors.length; i++) {
+                let lineaError = document.querySelector("small." + errors[i].name);                
+                lineaError.innerText = errors[i].msg;            
+            }            
+        }  
+    });
+
+    productSpecs.addEventListener ("blur", function(e) {
+
+        errors = [];
+            
+        if(nombre.value == '') {                
+            errors.push({ name: 'productSpecs', msg: 'Recordá ingresar, al menos, una especificación tecnica' });
+        } else if (nombre.value.length < 1) {                
+            errors.push({ name: 'productSpecs', msg: 'La especificación no puede ser solo una letra' });
+        } 
+        
+        if(errors.length == 0) {                
+            camposErrores[0].innerText = '';
+        } else {
+            for (let i = 0; i < errors.length; i++) {
+                let lineaError = document.querySelector("small." + errors[i].name);                
+                lineaError.innerText = errors[i].msg;            
+            }            
+        }  
+    });
+
+    productKeywords.addEventListener ("blur", function(e) {
+
+        errors = [];
+            
+        if(nombre.value == '') {                
+            errors.push({ name: 'productKeywords', msg: 'Debes ingresar una palabra clave' });
+        } else if (nombre.value.length < 1) {                
+            errors.push({ name: 'productKeywords', msg: 'La palabra clave no puede ser solo una letra' });
+        } 
+        
+        if(errors.length == 0) {                
+            camposErrores[0].innerText = '';
+        } else {
+            for (let i = 0; i < errors.length; i++) {
+                let lineaError = document.querySelector("small." + errors[i].name);                
+                lineaError.innerText = errors[i].msg;            
+            }            
+        }  
+    });
+
+    productPrice.addEventListener ("blur", function(e) {
+
+        errors = [];
+            
+        if(nombre.value == '') {                
+            errors.push({ name: 'productPrice', msg: 'Debes ingresar un valor' });
+        };
+        
+        if(errors.length == 0) {                
+            camposErrores[0].innerText = '';
+        } else {
+            for (let i = 0; i < errors.length; i++) {
+                let lineaError = document.querySelector("small." + errors[i].name);                
+                lineaError.innerText = errors[i].msg;            
+            }            
+        }  
+    });
+
+    productDiscount.addEventListener ("blur", function(e) {
+
+        errors = [];
+            
+        if(nombre.value == '') {                
+            errors.push({ name: 'productDiscount', msg: 'Debes ingresar un descuento' });
+        };
+        
+        if(errors.length == 0) {                
+            camposErrores[0].innerText = '';
+        } else {
+            for (let i = 0; i < errors.length; i++) {
+                let lineaError = document.querySelector("small." + errors[i].name);                
+                lineaError.innerText = errors[i].msg;            
+            }            
+        }  
+    });
+
+    productStock.addEventListener ("blur", function(e) {
+
+        errors = [];
+            
+        if(nombre.value == '') {                
+            errors.push({ name: 'productStock', msg: 'Debes ingresar el stock del producto' });
+        } else if (nombre.value.length <= 0) {                
+            errors.push({ name: 'productStock', msg: 'El stock no puede ser menor o igual a 0' });
+        } 
+        
+        if(errors.length == 0) {                
+            camposErrores[0].innerText = '';
+        } else {
+            for (let i = 0; i < errors.length; i++) {
+                let lineaError = document.querySelector("small." + errors[i].name);                
+                lineaError.innerText = errors[i].msg;            
+            }            
+        }  
+    });
+
+    productStockmin.addEventListener ("blur", function(e) {
+
+        errors = [];
+            
+        if(nombre.value == '') {                
+            errors.push({ name: 'productStockmin', msg: 'Debes indicar un stock mínimo' });
+        } else if (nombre.value.length <= 0) {                
+            errors.push({ name: 'productStockmin', msg: 'El stock minímo no puede ser igual a 0' });
+        } 
+        
+        if(errors.length == 0) {                
+            camposErrores[0].innerText = '';
+        } else {
+            for (let i = 0; i < errors.length; i++) {
+                let lineaError = document.querySelector("small." + errors[i].name);                
+                lineaError.innerText = errors[i].msg;            
+            }            
+        }  
+    });
+
+    productStockmax.addEventListener ("blur", function(e) {
+
+        errors = [];
+            
+        if(nombre.value == '') {                
+            errors.push({ name: 'productStockmax', msg: 'Debes indicar un stock máximo' });
+        } else if (nombre.value.length <= 0) {                
+            errors.push({ name: 'productStockmax', msg: 'El stock máximo no puede ser igual a 0' });
+        } 
+        
+        if(errors.length == 0) {                
+            camposErrores[0].innerText = '';
+        } else {
+            for (let i = 0; i < errors.length; i++) {
+                let lineaError = document.querySelector("small." + errors[i].name);                
+                lineaError.innerText = errors[i].msg;            
+            }            
+        }  
+    });
+
+
+
+
+});
