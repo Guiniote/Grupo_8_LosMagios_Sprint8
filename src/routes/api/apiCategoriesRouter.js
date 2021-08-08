003717bc6ce9eb6cd6404fd6213916c82dbe50c0
gabodmc <gabrielmc@live.com.ar>
2021-08-08 02:14:28 -0300

    Subo api para categorias - GMC

--- src/routes/api/apiCategoriesRouter.js
+++ src/routes/api/apiCategoriesRouter.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express.Router();
 const apiCategoriesController = require('../../controller/api/apiCategoriesController');
 
-//Rutas
-//Listado de marcas
 router.get('/', apiCategoriesController.list);
-//Cantidad de marcas
 router.get('/count', apiCategoriesController.count);
-//Detalle de una marca
 router.get('/:id', apiCategoriesController.detail);
 
 module.exports = router;
